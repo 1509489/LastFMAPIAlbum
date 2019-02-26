c27edc0b2a6b14859a3b09641bea2f55c2cdbee6
Justice <popystone@gmail.com>
2019-02-26 18:19:03 +0000

    Added implementation for the home screen presenter

--- app/src/main/java/com/pixelart/lastfmapialbum/ui/homescreen/HomeContract.kt
+++ app/src/main/java/com/pixelart/lastfmapialbum/ui/homescreen/HomeContract.kt
@@ -1,10 +1,12 @@
 package com.pixelart.lastfmapialbum.ui.homescreen
 
+import com.pixelart.lastfmapialbum.model.Album
+
 interface HomeContract {
     interface View{
-
+        fun showAlbums(albums: List<Album>)
     }
     interface Presenter{
-
+        fun getAlbums(album: String)
     }
 }
--- app/src/main/java/com/pixelart/lastfmapialbum/ui/homescreen/HomePresenter.kt
+++ app/src/main/java/com/pixelart/lastfmapialbum/ui/homescreen/HomePresenter.kt
@@ -1,4 +1,33 @@
 package com.pixelart.lastfmapialbum.ui.homescreen
 
-class HomePresenter(private val view: HomeContract.View): HomeContract.Presenter {
+import com.pixelart.lastfmapialbum.common.API_KEY
+import com.pixelart.lastfmapialbum.common.FORMAT
+import com.pixelart.lastfmapialbum.common.METHOD
+import com.pixelart.lastfmapialbum.model.AlbumResponse
+import com.pixelart.lastfmapialbum.network.NetworkService
+import io.reactivex.SingleObserver
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+
+class HomePresenter(private val networkService: NetworkService, private val view: HomeContract.View): HomeContract.Presenter {
+
+    override fun getAlbums(album: String) {
+        networkService.getAlbums(METHOD, album, API_KEY, FORMAT)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(object : SingleObserver<AlbumResponse>{
+                override fun onSuccess(t: AlbumResponse) {
+
+                }
+
+                override fun onSubscribe(d: Disposable) {
+
+                }
+
+                override fun onError(e: Throwable) {
+
+                }
+            })
+    }
 }
